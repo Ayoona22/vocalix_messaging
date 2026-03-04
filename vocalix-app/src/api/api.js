56cedb7f19c892587efd35d413836e33f22b5fa7
Ayoona22 <159051775+Ayoona22@users.noreply.github.com>
2026-03-04 10:43:38 +0530

    Update api.js

--- vocalix-app/src/api/api.js
+++ vocalix-app/src/api/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://navigation-export-projector-full.trycloudflare.com' });
+const api = axios.create({ baseURL: 'backend-url' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
 
 export default api;
